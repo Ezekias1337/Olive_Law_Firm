763f5dd57ea33016c16410849e9b28828fb2bc27
Frank Edwards <frankedwards50@gmail.com>
2023-07-17 23:45:44 -0700

    cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,13 @@ import { config } from "dotenv";
 config();
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 const cases = require("./routes/cases");
 const PORT = 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/cases", cases);
 
